6d46e04a9638c3983e2fbebed193c36a30c5740b
victor <victor.lavado15@hotmail.com>
2023-06-06 23:36:24 -0500

    product modific

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { useSelector } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navigation from "./components/Navigation";
+import CategoryPage from "./pages/CategoryPage";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import NewProduct from "./pages/NewProduct";
+import ProductPage from "./pages/ProductPage";
 import Signup from "./pages/Signup";
 
 function App() {
@@ -23,6 +25,9 @@ function App() {
               <Route path="/signup" element={<Signup />} />
             </>
           )}
+          <Route path="/product/:id" element={<ProductPage />} />
+          <Route path="/category/:category" element={<CategoryPage />} />
+
           <Route path="/new-product" element={<NewProduct />} />
 
           <Route path="*" element={<Home />} />
